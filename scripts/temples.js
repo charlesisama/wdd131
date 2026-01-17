d8aeb16858b31047704f32ff9782d15ff751436f
charlesisama <cisama@byupathway.edu>
2026-01-17 15:52:25 +0100

    updating the styles for small and larger view

---
+++ scripts/temples.js
@@ -0,0 +1,18 @@
+
+// SET CURRENT DATE AND LAST MODIFICATION
+const yearSpan = document.getElementById("currentyear");
+
+const lastModified = document.getElementById("lastModified");
+
+yearSpan.textContent = new Date().getFullYear ();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+
+// HAMBUGER CLICK EVENT
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+})
