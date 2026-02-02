89fb281492cdd5d03622ab3dc5c34a9d68b1a10a
charlesisama <cisama@byupathway.edu>
2026-02-03 00:46:57 +0100

    adding temple images to improve performance score

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -27,7 +27,7 @@ const temples = [
     dedicated: "2005, August, 7",
     area: 11500,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    "images/aba-nigeria-temple.webp"
   },
   {
     templeName: "Manti Utah",
@@ -35,7 +35,7 @@ const temples = [
     dedicated: "1888, May, 21",
     area: 74792,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    "images/manti-temple.webp"
   },
   {
     templeName: "Payson Utah",
@@ -43,7 +43,7 @@ const temples = [
     dedicated: "2015, June, 7",
     area: 96630,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    "images/payson-utah-temple.webp"
   },
   {
     templeName: "Yigo Guam",
@@ -51,7 +51,7 @@ const temples = [
     dedicated: "2020, May, 2",
     area: 6861,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    "images/yigo-guam-temple.webp"
   },
   {
     templeName: "Washington D.C.",
@@ -59,7 +59,7 @@ const temples = [
     dedicated: "1974, November, 19",
     area: 156558,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    "images/washington_dc_temple.webp"
   },
   {
     templeName: "Lima Perú",
@@ -67,7 +67,7 @@ const temples = [
     dedicated: "1986, January, 10",
     area: 9600,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    "images/lima-peru-temple.webp"
   },
   {
     templeName: "Mexico City Mexico",
@@ -75,7 +75,7 @@ const temples = [
     dedicated: "1983, December, 2",
     area: 116642,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    "images/mexico-city-temple.webp"
   },
   // Add more temple objects here...
    {
