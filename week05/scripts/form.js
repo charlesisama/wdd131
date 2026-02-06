4259782c2393e66c358cfeeaf36d6900adca3b1b
charlesisama <cisama@byupathway.edu>
2026-02-06 22:13:17 +0100

    form styling

---
+++ week05/scripts/form.js
@@ -0,0 +1,11 @@
+// SET CURRENT DATE AND LAST MODIFICATION
+const yearSpan = document.getElementById("currentyear");
+
+const lastModified = document.getElementById("lastModified");
+
+yearSpan.textContent = new Date().getFullYear ();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+
+//PRODUCT DATA
+
